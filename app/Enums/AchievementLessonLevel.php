1a3c6a98cd89b3b05faf84b16daa9e99ef700d54
Elef Rosales <earq14@gmail.com>
2024-01-24 23:19:24 -0500

    refactor: refactor achivement lesson level

--- app/Enums/AchievementLessonLevel.php
+++ app/Enums/AchievementLessonLevel.php
@@ -4,9 +4,9 @@ namespace App\Enums;
 
 enum AchievementLessonLevel: int {
     case First = 1;
-    case Five = 5;
-    case Ten = 10;
-    case TwentyFive = 25;
-    case Fifty = 50;
+    case Five = 2;
+    case Ten = 3;
+    case TwentyFive = 4;
+    case Fifty = 5;
 
 }
